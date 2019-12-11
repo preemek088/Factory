d2c096121a3cf26c3dc389b9d0e8b87fe399f41f
F_Olszowski <folszowski@student.agh.edu.pl>
2019-12-11 12:11:14 +0100

    Start Project

---
+++ cmake-build-debug/CMakeFiles/Factory.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Factory.dir/main.cpp.obj"
+  "Factory.exe"
+  "Factory.exe.manifest"
+  "Factory.pdb"
+  "libFactory.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Factory.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
