99ef124e7d336071e9bd5f88923b8222bbaa322b
Kacper <kmalczewski3@wp.pl>
2019-12-17 15:45:37 +0100

    pomozcie chlopaki

--- main.cpp
+++ main.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
 #include <deque>
+#include <vector>
+#include <set>
+#include <iterator>
 using elementID = int;
+
+static std::set<elementID> assigned_IDs;
+static std::set<elementID> freed_IDs;
 
 class Package
 {
     elementID ID;
 public:
-    Package(elementID ID_) {
+    Package(elementID ID_) //konstruktor
+    {
         ID = ID_;
     };
-    ~Package() = default;
-    elementID get_id() const {
+//    Package(Package&& package_) //konstruktor kopiujacy
+//    {
+//        ID = package_.ID;
+//
+//    };
+//    Package& operator=(Package&& other) {
+//
+//         ID = other.ID;
+//    };
+    ~Package() = default; //destruktor domyslny, tutaj przy zniszczeniu obiektu powinno przekazywac ID produktu do zbioru zwolnionych ID
+    elementID get_id() const
+    {
         return ID;
     }
 
@@ -24,18 +41,20 @@ enum PackageQueueType
 
 class IPackageStockPile
 {
+protected: //protected daje dostep klasie pochodnej do wszystkich pol, ale nie daje dostepu z zewnatrz(spoza klasy)
     virtual void push(Package&& Package) = 0;
     virtual bool empty() = 0;
-    virtual std::size_t size() {};
-
+    virtual std::size_t size() {return 1;};
+    ~IPackageStockPile() {};
 
 };
 
 class IPackageQueue
 {
+protected:
     virtual void pop() = 0;
     virtual PackageQueueType get_queue_type() = 0;
-
+    ~IPackageQueue() {};
 };
 
 class PackageQueue: public IPackageQueue
@@ -44,7 +63,7 @@ class PackageQueue: public IPackageQueue
 public:
     void push(Package&& package) {queue.push_back(package);};
 
-    bool empty() {if (queue.size()>0) return true;};
+    bool empty() {return (queue.size()==0);};
 
     std::size_t size() const {return queue.size();};
 
